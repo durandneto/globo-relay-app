ba6458273e98cf564f5a33e86a4898a7adb142ca
Jose Alves Durand Neto <durand.neto@gmail.com>
2020-05-25 22:18:35 -0700

    first build

---
+++ build/precache-manifest.16795be44491d30e6051feea3c634308.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78857e3807ec36b616e0e8deb9a1112a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8c82620504e8337ecc9",
+    "url": "/static/js/2.4e2e6c21.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4e2e6c21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389f760ebcc886a560a0",
+    "url": "/static/js/main.6169b3b0.chunk.js"
+  },
+  {
+    "revision": "69a34e6d397705eafbee",
+    "url": "/static/js/runtime-main.5d8e56ec.js"
+  }
+]);
